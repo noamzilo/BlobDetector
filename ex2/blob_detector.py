06a14cd7184ab73993d4461f2a8b0849d2cddccb
Noam <noams@epd-medical.com>
2019-11-16 18:47:38 +0200

    attempting non-max suppression

--- ex2/blob_detector.py
+++ ex2/blob_detector.py
@@ -32,6 +32,11 @@ class LogBlobDetector(object):
         self._scale_multiply_per_level = 2 ** 0.25
         self._max_min_threshold = 0
 
+    @staticmethod
+    def _non_max_suppression(centers_x, centers_y, radiuses):
+        assert len(centers_x) == len(radiuses)
+        return centers_X
+
     def find_local_maxima(self):
         color_image = np.zeros((self._h, self._w, 3), dtype=self._grayscale_image.dtype)
         color_image[:, :, 0] = self._grayscale_image
@@ -41,16 +46,23 @@ class LogBlobDetector(object):
 
         pyramids, scales, filt_sizes = self._pyramids.get_pyramids()
 
-        first_filter_size = filt_sizes[0]
-        last_filter_size = filt_sizes[-1]
-        avg_filter_size = np.median(filt_sizes)
-        arbitraty_size = filt_sizes[-3]
+        # first_filter_size = filt_sizes[0]
+        # last_filter_size = filt_sizes[-1]
+        # avg_filter_size = np.median(filt_sizes)
+        # arbitraty_size = filt_sizes[-3]
+        #
+        # data_max = filters.maximum_filter(pyramids, arbitraty_size)
+        # maxima_mask = np.logical_and((pyramids == data_max), data_max > self._max_min_threshold)
 
-        data_max = filters.maximum_filter(pyramids, arbitraty_size)
-        maxima_mask = np.logical_and((pyramids == data_max), data_max > self._max_min_threshold)
-        maxima_locations = np.where(maxima_mask)
+        maxima = np.max(pyramids)
+        maxima_inds = np.where(maxima == pyramids)
+        filt_sizes = np.array(filt_sizes)
+        maxima_scales = filt_sizes[np.array(maxima_inds[2])]
+        true_max = self._non_max_suppression(centers_x = maxima[maxima_inds][1], centers_y = maxima[maxima_inds][0], radiuses=maxima_scales)
 
-        for maxima_locations_x, maximum_location_y, mask_ind in zip(maxima_locations[1], maxima_locations[0], maxima_locations[2]):
+        true_max_locations = np.where(maxima_mask)
+
+        for maxima_locations_x, maximum_location_y, mask_ind in zip(true_max_locations[1], true_max_locations[0], true_max_locations[2]):
             cv2.circle(color_image, (maxima_locations_x, maximum_location_y), filt_sizes[mask_ind], (0, 255, 0), 1)
 
         cv2.imshow("local_maxima on original", color_image)
