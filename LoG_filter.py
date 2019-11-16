6062b7eb46de0b28f619f896598f045c5049c7c6
Noam <noams@epd-medical.com>
2019-11-16 02:16:42 +0200

    cosmetics

--- LoG_filter.py
+++ LoG_filter.py
@@ -11,24 +11,25 @@ import sys
 
 def log_filt(ksize, sig):
     std2 = float( sig**2 )
-    x = np.arange( -(ksize-1)/2, (ksize-1)/2+1, 1)
-    y = np.arange( -(ksize-1)/2, (ksize-1)/2+1, 1)
+    x = np.arange(-(ksize - 1) / 2, (ksize - 1) / 2 + 1, 1)
+    y = np.arange(-(ksize - 1) / 2, (ksize - 1) / 2 + 1, 1)
     X, Y = np.meshgrid(x, y)
-    arg = -(X*X + Y*Y)/(2*std2);
+    arg = -(X*X + Y*Y)/(2*std2)
     
-    h = np.exp(arg);
+    h = np.exp(arg)
     eps = sys.float_info.epsilon
-    h[h < eps*np.max(h)] = 0;
+    h[h < eps*np.max(h)] = 0
     
     sumh = np.sum(h)
     if sumh != 0:
-       h  = h/sumh
+       h = h/sumh
        
-       # now calculate Laplacian     
+    # now calculate Laplacian
     h1 = h*(X*X + Y*Y - 2*std2)/(std2**2);
     h = h1 - np.sum(h1)/(ksize*ksize) # make the filter sum to zero
   
     return h
+
 
 if __name__ == "__main__":
     sigma = 2
